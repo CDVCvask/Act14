b0df214a2343287decf7a0ba5ec7c0685f80bfed
CDVC <cesarcamposeco415@gmail.com>
2025-08-08 10:08:54 -0600

    Ingreso

--- Race.py
+++ Race.py
@@ -6,13 +6,32 @@ def Menu():
     print("4.Salir")
 allow = False
 allow1 = False
+participants = {}
+count = 0
 while allow == False:
     Menu()
     opt = int(input("Ingrese la opción que desee: "))
     print(" ")
     match opt:
         case 1:
+            check
             num = int(input("Cuantos participantes desea ingresar: "))
+            if num <= 0:
+                print("La cantidad ingresada no es valida")
+            else:
+                for i in range(num):
+                    code = f"D{count}"
+                    name = input("Ingrese el nombre completo del participante: ")
+                    if name == "":
+                        print("No puede dejar este espacio vacio")
+                    else:
+                        category = input("Ingrese el categoria del participante: ")
+                        check = category.lower()
+                        if check == "juvenil" or check == "adulto" or check == "master":
+                            participants[name] = num
+                        else:
+                            print("La categoría no es valida solo se aceptan: Juvenil, Adulto y Master")
+                    count = count + 1
         case 2:
             if allow1 == False:
                 print("Aún no hay ningún dato")
